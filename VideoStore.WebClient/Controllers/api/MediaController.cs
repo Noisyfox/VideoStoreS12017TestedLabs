2c87a5ef3fafc04462990ce0f7a2b2340d64eeda
noisyfox <timemanager.rick@gmail.com>
2017-04-11 00:33:03 +1000

    Add restapi

---
+++ VideoStore.WebClient/Controllers/api/MediaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideoStore.Services.Interfaces;
+using VideoStore.Services.MessageTypes;
+
+namespace VideoStore.WebClient.Controllers
+{
+    public class MediaController : ApiController
+    {
+        private ICatalogueService CatalogueService
+        {
+            get
+            {
+                return ServiceFactory.Instance.CatalogueService;
+            }
+        }
+
+        public IEnumerable<Media> GetAllMedia()
+        {
+            return CatalogueService.GetMediaItems(0, int.MaxValue);
+        }
+    }
+}
